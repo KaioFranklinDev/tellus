e457ad80f45903c7688217b57775d0491e1f5658
KaioFranklinDev <kaio.franklin.dev@gmail.com>
2024-10-08 11:36:48 -0300

    criando paleta de cor

---
+++ src/app/collors/page.tsx
@@ -0,0 +1,12 @@
+export default function Collors() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className=" w-1/4">
+          <p className="bg-primary h-20">Primary #03304F</p>
+          <p className="bg-primarylow">Primary Low #084C77</p>
+          <p className="bg-secondary">Secondary #fff</p>
+          <p className="bg-destac">destac #FEDA15</p>
+        </div>
+      </div>
+    );
+  }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#03304F',
+        primarylow: '#084C77',
+        secondary: '#fff',
+        destac: '#FEDA15'
+
       },
     },
   },
