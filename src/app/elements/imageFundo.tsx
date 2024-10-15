f8a53e3eae31c91155f62d3b0b17441174993d94
KaioFranklinDev <kaio.franklin.dev@gmail.com>
2024-10-15 05:54:34 -0300

    dev first container

--- src/app/elements/imageFundo.tsx
+++ src/app/elements/imageFundo.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function ImageFundo() {
     return (
-        <div className="absolute  bg-blue-600/90 w-screen h-[800px]" >
+        <div className="absolute w-screen h-[800px] -z-20" >
             <Image alt="logo" src={'/imgs/vistaazul.jpg'} layout="fill" objectFit="cover" objectPosition="bottom left" className="-z-40" />
+            <div className="w-screen h-[800px] bg-primary/80" />
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Form from "./elements/form";
 import Header from "./elements/header";
 import ImageFundo from "./elements/imageFundo";
 
@@ -6,17 +7,24 @@ export default function Home() {
     <div className="h-screen text-secondary">
       <Header />
       <ImageFundo />
-      <div className="flex justify-center items-center h-full">
-        <div className="bg-red-500 h-52 w-[550px]">
-          <div className="text-[60px] text-justify">
-            Coloque sua energia no lugar certo!
+      <div className="flex gap-14 justify-center pt-16 h-full ">
+        <div className=" w-[550px]">
+          <div className=" text-justify text-[30px]">
+            <p className="font-bold text-[20px]">Pra sua casa</p>
+            <p className=" pt-6 font-bold text-[60px] leading-none">Prospere desde a 1ª conta de luz</p>
+            <p className="pt-6"><span className="font-bold">Contrate a Tellus</span> para a sua casa e garanta economia todo mês e a 1ª fatura grátis</p>
+            <div className="text-nowrap pl-8 text-[25px]">
+              <p>&rsaquo; <span className="font-bold">Garantimos seu desconto</span> todos os meses</p>
+              <p>&rsaquo; Atendimento do seu jeito</p>
+              <p>&rsaquo; Sem custo de instalação ou investimento inicial</p>
+            </div>
           </div>
         </div>
-        <div className="bg-red-300 h-52 w-[700px]">
-          aaaaaaaaaa
+        <div className="h-[600px] w-[500px]">
+          <Form />
         </div>
 
-      </div> 
+      </div>
 
     </div>
   );
